a7afe13f82bc291be92604acf8b4586c64a65634
Mustafa Furkan BİLEN <furkanbilen4@gmail.com>
2022-11-26 14:52:31 +0300

    SumPair of List added

---
+++ src/hackerrank/SumPairList.java
@@ -0,0 +1,33 @@
+
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Find whic element pairs sum is equal to given sum
+ * @author Mustafa Furkan BİLEN <mustafafurkanbilen@gmail.com>
+ * @since Nov 26, 2022 2:35:37 PM
+ */
+public class SumPairList {
+
+	public static void calculate(int sum, int[]arr) {
+		Map<Integer, Integer> hm = new HashMap<>();
+		for(int i=0;i<arr.length;i++)
+			hm.put(arr[i], i);
+		for (int i = 0; i < arr.length; i++) {
+			Integer key = sum-arr[i];
+			Integer value = hm.get(key);
+			
+			if(hm.containsKey(key)&&i<value)
+				System.out.println(i+"-"+value);
+		}
+	}
+	public static void main(String[] args) {
+		
+		int arr [] = new int[] {1,2,3,5,9,8,6};
+		calculate(11, arr);
+		
+	}
+
+}

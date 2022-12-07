922f34df5bc57715aa43467e990cb6755773f38c
Mustafa Furkan Bilen <36642499+mfb14@users.noreply.github.com>
2022-12-07 07:44:31 +0300

    Day Of Programmer added

--- src/hackerrank/DayOfProgrammer.java
+++ src/hackerrank/DayOfProgrammer.java
@@ -1,9 +1,17 @@
 
+package hackerrank;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
 
-
-
-
+/**
+ * @link https:https://www.hackerrank.com/challenges/day-of-the-programmer
+ * @author Mustafa Furkan BİLEN <mustafafurkanbilen@gmail.com>
+ * @since December 7, 2022 7:16:41 AM
+ */
+public class DayOfProgrammer {
+   
    public static String dayOfProgrammer(int year) {
         
         if(year>1918){
@@ -15,3 +23,13 @@
         
         return year%4==0?"12.09."+year:"13.09."+year;
     }
+	public static void main(String[] args) {
+	
+      System.out.printlin(dayOfProgrammer(1900));
+	}
+
+}
+
+
+
+

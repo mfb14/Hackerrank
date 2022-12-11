9834a4c1bca4cab93e4bf807ea81a9f6a3edaef2
Mustafa Furkan BİLEN <furkanbilen4@gmail.com>
2022-12-11 21:28:03 +0300

    TwoSum with HashMap added

--- src/hackerrank/TwoSum.java
+++ src/hackerrank/TwoSum.java
@@ -1,6 +1,9 @@
 
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  * @author Mustafa Furkan BİLEN <mustafafurkanbilen@gmail.com>
@@ -19,9 +22,23 @@ public class TwoSum {
 		throw new IllegalArgumentException();
 		
 	}
+	
+	public static int[] twoSum(int list[],int target) {
+		Map<Integer,Integer> map = new HashMap<>();
+		
+		for(int i= 0;i<list.length;i++) {
+			
+			int temp = target-list[i];
+			if(map.containsKey(temp))
+				return new int[] {map.get(temp),i};
+			
+			map.put(list[i], i);
+		}
+		throw new IllegalArgumentException();
+	}
 	public static void main(String[] args) {
 		
-		int [] result = findTwoSum(new int[] {3, 1, 5, 7, 5, 9 }, 10);
+		int [] result = twoSum(new int[] {3, 1, 5, 7, 5, 9 }, 10);
 		
 		System.out.println(result[0]+","+result[1]);
 
